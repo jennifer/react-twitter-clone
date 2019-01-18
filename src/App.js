43ebe0c8318cdcd8710118e09970c1d11efe9f93
jennifer kerssen <jjkerssen@gmail.com>
2019-01-17 20:48:14 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MessageList from './MessageList';
+import * as messagesApi from './messagesApi';
+
+const MessageInput = ({ handleInput, newMessageText, submitMessage }) => {
+  return (
+    <div>
+      <input 
+        onChange={handleInput}
+        value={newMessageText}
+        type='text' 
+        placeholder='Message' 
+      ></input>
+      <button onClick={submitMessage}>Submit</button>
+    </div>
+  )
+}
 
 class App extends Component {
+  state = {
+    messages: [],
+  };
+
+  componentDidMount() {
+    messagesApi.getMessages()
+      .then(res => this.setState(
+        { messages: res.messages },
+        () => console.log(this.state),
+      )
+    );
+  }
+
+  handleInput = ({ target }) => {
+    this.setState({ newMessageText: target.value });
+  }
+
+  submitMessage = () => {
+    const { messages } = this.state;
+    const newMessage = {
+      messages_id: messages.length + 1,
+      handle: 'Jennifer',
+      text: this.state.newMessageText,
+      stars: 10,
+      timestamp: Date.now()
+    };
+    messages.unshift(newMessage);
+    this.setState({ messages: messages, newMessageText: '' })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>React Coding Night</h1>
+        <MessageInput
+          handleInput={this.handleInput}
+          newMessageText={this.state.newMessageText}
+          submitMessage={this.submitMessage}
+        />
+        <MessageList messages={this.state.messages} />
       </div>
     );
   }
---
+++ src/MessageList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+
+
+const Message = ({ message }) => {
+    const timestamp = new Date(message.timestamp);
+    
+    return (
+        <div className='Message'>
+            <p>@{message.handle}</p>
+            <p>{message.text}</p>
+            <div>
+                <p><i className="fa fa-star" /> {message.star_count}</p>
+                <p>{timestamp.toDateString()}</p>
+            </div>
+        </div>
+    );
+};
+
+class MessageList extends Component {
+    render() {
+
+        const { messages } = this.props;
+
+        return  (
+            <div className='MessageList'>
+                {messages.map(message => 
+                    <Message 
+                        key={message.messages_id} 
+                        message={message} 
+                    />
+                )}
+            </div>
+        )
+    }
+}
+
+export default MessageList;
